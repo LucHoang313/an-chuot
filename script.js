364167537730b0823dd75c9d8aee888e5f556e72
LucHoag212 <hluc3438@gmail.com>
2024-06-07 21:11:08 +0700

    change

--- script.js
+++ script.js
@@ -9,9 +9,25 @@ function changeImage() {
         '240p': 'images/240p.png',
         '480p': 'images/480p.png',
         '720p': 'images/720p.png',
-        '1080p': 'images/1080p.png'
+        '1080p': ['images/1.png', 'images/2.png', 'images/3.png', 'images/4.png', 'images/5.png']
     };
 
-    // Change image source based on selected quality
-    qualityImage.src = qualityImageMap[selectedQuality];
+    // If 1080p is selected, cycle through images
+    if (selectedQuality === '1080p') {
+        let index = 0;
+        const images = qualityImageMap[selectedQuality];
+
+        function showNextImage() {
+            if (index < images.length) {
+                qualityImage.src = images[index];
+                index++;
+                setTimeout(showNextImage, 1000); // Change image every 1 second
+            }
+        }
+
+        showNextImage();
+    } else {
+        // Change image source based on selected quality
+        qualityImage.src = qualityImageMap[selectedQuality];
+    }
 }
